0f84451daabd6341bfae5a008adbb25165792bfb
Matt Gaunt <matt@gauntface.co.uk>
2017-05-12 11:29:04 -0700

    Toast only shown if current page changed

--- src/themes/scripts/sw-controller.js
+++ src/themes/scripts/sw-controller.js
@@ -3,9 +3,12 @@ window.addEventListener('load', function() {
     if (window.BroadcastChannel) {
       const updateChannel = new window.BroadcastChannel('precache-updates');
       updateChannel.addEventListener('message', (event) => {
-        console.log(event);
-        window.__workbox.toast.show(
-          'This page has been updated. Please refresh the page.');
+        if (event.data.payload.updatedUrl === location.href) {
+          window.__workbox.toast.show(
+            'This page has been updated. Please refresh the page.');
+        } else {
+          console.log('Precache has updated in the background: ', event);
+        }
       });
     }
     navigator.serviceWorker.register('/sw.js')
