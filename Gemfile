aabe65b53e88ac72741cfe49df5361ac459df2f5
Matt Gaunt <matt@gauntface.co.uk>
2017-08-22 16:36:56 -0700

    Moving files our

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source "http://rubygems.org"
-
-gem 'jekyll', '>=3.1.6'
-gem 'kramdown'
--- gulp-tasks/scripts.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const gulp = require('gulp');
-
-gulp.task('scripts', () => {
-  return gulp.src(global.config.src + '/**/*.js')
-  .pipe(gulp.dest(global.config.dest));
-});
